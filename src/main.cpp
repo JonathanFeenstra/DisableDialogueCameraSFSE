04618b6580786133426d0829a268b4507c7dcc3e
Jonathan Feenstra <26406078+JonathanFeenstra@users.noreply.github.com>
2023-10-06 22:39:54 +0200

    Use ToggleControlsDriven command on dialogue menu close instead of hacky workaround

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,11 @@ namespace Hook
 {
 	typedef void(WINAPI* pFunc)(std::uint64_t a_1, RE::CameraState);
 	inline pFunc originalFunction;
+
 	inline static void replacementFunction(std::uint64_t a_1, RE::CameraState a_cameraState)
 	{
-		const auto currentState = *stl::adjust_pointer<RE::CameraState>(RE::PlayerCamera::GetSingleton()->currentState, 0x50);
-		originalFunction(a_1, a_cameraState);
-		if (a_cameraState == RE::CameraState::kDialogue) {
-			originalFunction(a_1, currentState);
+		if (a_cameraState != RE::CameraState::kDialogue) {
+			originalFunction(a_1, a_cameraState);	
 		}
 	}
 
@@ -26,11 +25,53 @@ namespace Hook
 	}
 };
 
+namespace RE // Copied from https://github.com/Bobbyclue/Console-Command-Runner-SF/blob/8a11a81bc88440ad7f9023f406fcbf377e98d75d/src/Hooks.cpp#L5-L19
+{
+	void* BGSScaleFormManager()
+	{
+		static REL::Relocation<void**> singleton{ REL::ID(879512) };
+		return *singleton;
+	}
+
+	void ExecuteCommand(void* a_scaleformManager, const char* a_command)
+	{
+		using func_t = decltype(&ExecuteCommand);
+		REL::Relocation<func_t> func{ REL::ID(166307) };
+		func(a_scaleformManager, a_command);
+	}
+}
+
+namespace EventSink
+{
+	class MenuOpenCloseEventSink final :
+		public RE::BSTEventSink<RE::MenuOpenCloseEvent>,
+		public ISingleton<MenuOpenCloseEventSink>
+	{
+		RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent& a_event, RE::BSTEventSource<RE::MenuOpenCloseEvent>*) override
+		{
+			if (a_event.menuName == "DialogueMenu" && !a_event.opening) {
+				RE::ExecuteCommand(RE::BGSScaleFormManager(), "player.ToggleControlsDriven 1");
+			}
+
+			return RE::BSEventNotifyControl::kContinue;
+		}
+	};
+
+	static void Install()
+	{
+		if (const auto ui = RE::UI::GetSingleton()) {
+			ui->RegisterSink(MenuOpenCloseEventSink::GetSingleton());
+			logger::info("Registered MenuOpenCloseEventSink");
+		}
+	}
+};
+
 static void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept
 {
 	switch (a_msg->type) {
 	case SFSE::MessagingInterface::kPostLoad:
 		Hook::Install();
+		EventSink::Install();
 		break;
 	default:
 		break;
